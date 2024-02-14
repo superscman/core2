4de6a247619f717f2c3433a1014a9b84ef9d367b
superscman <vdcf2000@gmail.com>
2024-02-14 21:59:07 +0900

    처음 들었을때 보다 많은 것들이 보인다.

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,20 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+
+        Member mamber = new Member(1L, "mamberA", Grade.VIP);
+        memberService.join(mamber);
+
+        Member findeMember = memberService.findMember(1L);
+        System.out.println("mamber = " + mamber.getGrade());
+        System.out.println("findeMember = " + findeMember.getGrade());
+    }
+}
---
+++ src/main/java/hello/core/member/Grade.java
@@ -0,0 +1,12 @@
+package hello.core.member;
+
+public enum Grade {
+
+    VIP("vip");
+
+    private String grade;
+
+    Grade(String vip) {
+
+    }
+}
